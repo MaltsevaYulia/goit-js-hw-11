7ca3bb4b0380d1f656313a142111fee7ad86988d
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-02-07 18:06:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,45 @@ console.log(refs.gallery);
 
 refs.form.addEventListener('submit', onSearch);
 
-function onSearch(params) {
-  console.log('aaaaaaaa');
+function onSearch(evt) {
+  evt.preventDefault();
+  const searchQuery = evt.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery);
+    fetchGallery(searchQuery)
+      .then(res => res.hits)
+      .then(createMarkup);
+        //.then(res => console.log(res));
 }
 
-fetchGallery('yellow+flower').then(res => console.log(res));
-
+function createMarkup(images) {
+   let markup = '';
+    markup = images.map(({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+    }).join('');
+    console.log(markup);
+    refs.gallery.insertAdjacentHTML('beforeend', markup)
+}
