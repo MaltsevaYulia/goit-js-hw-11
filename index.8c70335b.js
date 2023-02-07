82047e1e8e9b60864c2428d7e52d7dca95fe32f5
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-02-07 11:14:35 +0000

    Deploying to gh-pages from @ MaltsevaYulia/goit-js-hw-11@e8d5068ff4c939e7f8a5aac842388ffc473d2e58 🚀

--- index.8c70335b.js
+++
@@ -1,2 +0,0 @@
-console.log("Это самое начало проверка");
-//# sourceMappingURL=index.8c70335b.js.map
--- index.a6b498d0.js
+++
@@ -1,2 +0,0 @@
-console.log("Это самое начало проверка");
-//# sourceMappingURL=index.a6b498d0.js.map
